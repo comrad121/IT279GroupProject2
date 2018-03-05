09cfe9ebdd84ec1db0cedc223c7283a3c29e1c53
Jarred Binder <jarredbinder@gmail.com>
2018-03-05 02:16:56 +0000

    Update InputOutputExample.cpp

--- InputOutputExample.cpp
+++ InputOutputExample.cpp
@@ -1,43 +1,40 @@
-#include <cstdlib>
-#include <iostream>
-#include <fstream>
-#include <string>
-
-#include "LinkedList.h"
-
-using namespace std;
-
 int main(int argc, char *argv[]) {
     if(argc == 1){
         cout<<"No command line argument given.\n"
                 "Please provide the input file name as an argument.\n"
-                "Ex: TODO:[Your program's name] file.txt\n";
+                "Ex: TODO:[your program's name] file.txt\n";
         return -1;
     }
-
+    
+    LinkedList list;
     ifstream inputFile;
     ofstream outputFile;
     string inputFileName(argv[1]);
-    string outputFileName = inputFileName.append("_sorted");
+    string outputFileName = inputFileName;
+    outputFileName.append("_sorted.txt");
+    long inputNum;
     
     // File input. It reads every line of the argued file into a long, then
     // adds it to the list.
     inputFile.open(inputFileName);
-    long inputNum;
-    while(inputFile >> inputNum){
-		// TODO: add inputNum your array/vector here, ex: list.push(inputNum);
-	}
-    inputFile.close();
+    if(inputFile.is_open()){
+        while(inputFile >> inputNum){
+            // TODO: add each inputNum to your data structure, e.g. list.push(inputNum);
+        }
+        inputFile.close();
+    }else{
+        cout<<"Unable to open "<<inputFileName<<endl;
+        return -1;
+    }
     
-    // TODO: run your sorting algorithm here
+    // TODO: perform sort here
     
     // File output. It prints every element of list onto its own line
     // in a file named "[inputFileName]_sorted.txt"
     outputFile.open(outputFileName);
-    node temp = list.getHead();
-    while(temp->next != NULL){
-        outputFile<<temp->data<<endl;
-        temp = temp->next;
+    while(list.getSize() > 0){
+        outputFile<<list.readHead()<<endl;
+        list.deleteHead();
     }
     outputFile.close();
     
